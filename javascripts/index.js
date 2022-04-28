88c35da1b6908beebac0247b23c51a0d27039491
Fedecolombo1 <colombofederico17@gmail.com>
2022-04-28 18:18:30 -0300

    arreglo

--- javascripts/index.js
+++ javascripts/index.js
@@ -20,4 +20,46 @@ window.addEventListener("load", function(){
     $(".violeta").toggleClass("abierto");
 		$(".negro").toggleClass("abierto");
   });
+
+
+  const btn = document.getElementById('button');
+                const name = document.getElementById('name');
+                const email = document.getElementById('email');
+                const phone = document.getElementById('phone');
+                const message = document.getElementById('message');
+                const plan1 = document.getElementById('plan1');
+                const plan2 = document.getElementById('plan2');
+                const plan3 = document.getElementById('plan3');
+                const plan4 = document.getElementById('plan4');
+
+        /*EmailJS*/
+        document.getElementById('form')
+        .addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        btn.value = 'Sending';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_iy8ztim';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Sent';
+
+                setTimeout(() => {
+                    name.value = '';
+                    email.value = '';
+                    phone.value = '';
+                    message.value = '';
+                    plan1.classList.remove("seleccionado");
+                    plan2.classList.remove("seleccionado");
+                    plan3.classList.remove("seleccionado");
+                    plan4.classList.remove("seleccionado");
+                    btn.value = 'Lets get in contact';
+                },2500);
+            }, (err) => {
+            btn.value = 'Lets get in contact';
+            alert(JSON.stringify(err)+ " - PORFAVOR CONTACTARSE POR WPP");
+            });
+        });
 })
