40017f2d25d5020fde4f70486455cb7dbf6ae4bd
Fedecolombo1 <colombofederico17@gmail.com>
2022-04-28 14:10:22 -0300

    js y cambios form

--- javascripts/index.js
+++ javascripts/index.js
@@ -20,33 +20,4 @@ window.addEventListener("load", function(){
     $(".violeta").toggleClass("abierto");
 		$(".negro").toggleClass("abierto");
   });
-
-  $("#plan1").click(function(){
-		$(this).toggleClass("seleccionado");
-    $("#plan2").removeClass("seleccionado");
-    $("#plan3").removeClass("seleccionado");
-    $("#plan4").removeClass("seleccionado");
-		$("#planSeleccionado").val("onePage");
-	});
-  $("#plan2").click(function(){
-		$(this).toggleClass("seleccionado");
-    $("#plan1").removeClass("seleccionado");
-    $("#plan3").removeClass("seleccionado");
-    $("#plan4").removeClass("seleccionado");
-		$("#planSeleccionado").val("fullPage");
-	});
-  $("#plan3").click(function(){
-		$(this).toggleClass("seleccionado");
-    $("#plan2").removeClass("seleccionado");
-    $("#plan1").removeClass("seleccionado");
-    $("#plan4").removeClass("seleccionado");
-		$("#planSeleccionado").val("eComnmerce");
-	});
-  $("#plan4").click(function(){
-		$(this).toggleClass("seleccionado");
-    $("#plan2").removeClass("seleccionado");
-    $("#plan3").removeClass("seleccionado");
-    $("#plan1").removeClass("seleccionado");
-		$("#planSeleccionado").val("custom");
-	});
 })
